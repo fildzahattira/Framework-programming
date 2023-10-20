532f6365972382b95b6952ec0ff3a319f8a299d3
fildzahattira <fildzahattira560@gmail.com>
2023-10-20 15:50:04 +0700

    config index.php

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,9 @@ $config['base_url'] = 'http://localhost/ci2023/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+// $config['index_page'] = 'index.php';
+$config['index_page'] = '';
+
 
 /*
 |--------------------------------------------------------------------------
